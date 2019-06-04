ceddce12914d94393f5bf2e97d23c579f957cc05
JackFlanagan93 <JackFlanagan93@outlook.com>
2019-06-04 16:36:29 +0100

    Account class filled, constructor and getters/setters added

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -8,4 +8,50 @@ public class Account {
 	//A First Name
 	//A last Name
 	
+	private int id;
+	private int accountNumber;
+	private String firstName;
+	private String lastName;
+	private int accountCounter = 0;
+		
+	public Account(int id, int accountNumber, String firstName, String lastName) {
+	
+		this.id = ++accountCounter;
+		this.accountNumber = accountNumber;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
 }
